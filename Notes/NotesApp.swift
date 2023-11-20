30c70d70bd2b9e3498bc2b4b014ec6e893811cd2
SURYAKANTSHARMA <suryakantsharma84@gmail.com>
2023-11-20 06:42:23 +0530

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Surya on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
